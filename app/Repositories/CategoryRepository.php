e77cc135e3690a178538a44476c7c6f977791b92
M-Hidayatullah <hidayatullahblk@gmail.com>
2025-03-05 02:47:44 +0800

    feat: creat service repository category

---
+++ app/Repositories/CategoryRepository.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\Category;
+
+class CategoryRepository
+{
+    public function getAll()
+    {
+        return Category::all();
+    }
+
+    public function findById($id)
+    {
+        return Category::findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return Category::create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $category = Category::findOrFail($id);
+        $category->update($data);
+        return $category;
+    }
+
+    public function delete($id)
+    {
+        $category = Category::findOrFail($id);
+        return $category->delete();
+    }
+}
