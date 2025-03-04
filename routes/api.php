6555b4d98abb1aa2b4747d7d7a4874b0b5bab141
m-hidayatullahh <dayattdev@gmail.com>
2025-03-05 02:53:18 +0800

    routes api

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,5 @@ Route::post('/categories', [CategoryController::class, 'store']);
 Route::get('/categories/{id}', [CategoryController::class, 'show']);
 Route::put('/categories/{id}', [CategoryController::class, 'update']);
 Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+#postman
