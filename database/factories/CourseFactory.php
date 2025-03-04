c2c3bb7d9c9b7f6b4ccb2c0d7cffb1d1c9bceae9
M-Hidayatullah <hidayatullahblk@gmail.com>
2025-03-05 02:39:15 +0800

    feat: add factories

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Course;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    protected $model = Course::class;
+
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph(),
+            'category_id' => Category::factory(),
+        ];
+    }
+}
